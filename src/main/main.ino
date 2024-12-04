e0aefa4595b32c16fe27fefbbb399202b7761851
jmabt <157436394+jmabt@users.noreply.github.com>
2024-12-04 13:08:06 -0500

    Commenting

--- src/main/main.ino
+++ src/main/main.ino
@@ -6,16 +6,32 @@
 
 #define SLOTX 6
 #define CYCLEX 4
-#define SLOT_T 5000
 
 int newState = 1;
 int flag = 0;
 
-// nov 27th
+//last major code change nov 27th
 // - no servo because uno keeps writing gibberish, Mega 2560 works oddly
 // - no microsd card based speech, not enough memory
 // - stability increased significantly from pulling SD card libs out (free memory skyrocketed)
 
+/***
+This main.ino file is the main loop for the EzVOC ventilation system project.
+The main file here handles state changes, and calls various functions using a 
+table based realtime scheduler. The size of the table is SLOTX * CYCLEX. The
+member functions in this file call functions in other classes, namely Sensors, UI,
+Button, Control. The member functions themselves are called by a member function, 
+ttable. Ttable is called by nested for loops in the loop() function. The member 
+functions generally only call functions in other classes. Very few of them actually
+do any sort of major calculation or checking - generally they just call functions and
+occasionally pass values. 
+
+The ttable function is used in place of a basic loop based function. It increases 
+responsiveness by checking button presses significantly more often, and helps reduce
+state deboucing by calling the state change functions in UI.h only once per loop. 
+The table method was inspired by an assignment completed in UB's CSE-321 course 
+in the Fall 2024 semester, and modified to work for the arduino uno. 
+***/ 
 
 UI d;
 sensors s;
@@ -86,7 +102,7 @@ void loop() {
       but since only 2 pins that were in use have hardware interrupts, this is the best alternative.*/
 
       for (int slot=0; slot < SLOTX; slot++){
-        for (int cycle=0; cycle<CYCLEX; cycle++){
+        for (int cycle=0; cycle < CYCLEX; cycle++){
           (*ttable[slot][cycle])();
         }
       }
